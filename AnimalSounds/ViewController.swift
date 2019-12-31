511256268714af1f175df3f0a5f2ce9d553d68a2
Alikhan Batchaev <loko-han@mail.ru>
2019-12-31 13:55:57 +0300

    I do not know how I loaded the repository without code. Just noticed today. Added code ... basically everything is added

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let cowSound = SimpleSound(named: "moo")
+    let dogSound = SimpleSound(named: "woof")
+    let catSound = SimpleSound(named: "meow")
+    
+    
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func catButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Meow!"
+        catSound.play()
+    }
+    
+    @IBAction func dogButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Woof!"
+        dogSound.play()
+    }
+    
+    @IBAction func cowButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Moo!"
+        cowSound.play()
+    }
+    
 }
 
